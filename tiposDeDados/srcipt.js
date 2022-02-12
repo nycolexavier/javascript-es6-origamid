1a6b87be8406fabc6bc77444300f17514b98adb3
Nycole Xavier <nycole.xavierr@gmail.com>
2022-02-11 20:50:18 -0400

    ➕ tipos de dados

---
+++ tiposDeDados/srcipt.js
@@ -0,0 +1,20 @@
+// var nome = "André";
+// var idade = 19;
+// var time = null;
+// console.log(typeof time);
+
+// Exercícios
+
+var nome = "André";
+
+var numero = "19";
+
+var idade = 19;
+
+var sobrenome = "Xavier";
+
+console.log(nome + sobrenome);
+
+var frase = "It's time";
+
+console.log(typeof frase);
