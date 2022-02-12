ec182a642956e36bda8a0b25ecc3e1f69dd9d03b
Nycole Xavier <nycole.xavierr@gmail.com>
2022-02-12 08:26:17 -0400

    ➕ numeros e operadores 2

--- numerosEOperadores/script.js
+++ numerosEOperadores/script.js
@@ -4,3 +4,25 @@ console.log(idade);
 
 var testeNaN = "Isso é 100" / 2;
 console.log(isNaN(testeNaN));
+
+// Exercício
+
+var total = 10 + (5 * 2) / 2 + 20;
+console.log(total);
+
+var numero10 = "teste" - 10;
+var numero100 = "outroTeste" * 25;
+console.log(numero10);
+console.log(numero100);
+
+var somar = "200";
+console.log(+somar + 50);
+
+var incremento = 5;
+console.log(++incremento);
+
+var numero1 = +"80" / 2;
+var unidade = "kg";
+var peso = numero1 + unidade;
+var pesoPorDois = peso;
+console.log(pesoPorDois);
