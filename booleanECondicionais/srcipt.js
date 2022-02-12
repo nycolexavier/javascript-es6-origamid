05c1a8ab555e0ee8508f871cd481fb139ebb4beb
Nycole Xavier <nycole.xavierr@gmail.com>
2022-02-12 09:52:30 -0400

    ➕ boolean e condicionais

---
+++ booleanECondicionais/srcipt.js
@@ -0,0 +1,18 @@
+var possuiGraduacao = false;
+var possuiDoutorado = false;
+
+if (possuiGraduacao) {
+  console.log("É verdadeiro");
+} else if (possuiDoutorado) {
+  console.log("É falso");
+} else {
+  console.log("Não possui nada");
+}
+
+var nome = "André";
+
+if (nome) {
+  console.log(nome);
+} else {
+  console.log("Nome não existe");
+}
