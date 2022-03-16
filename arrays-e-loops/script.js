2d000aae4cff29c236d692ec851adbeb769c471d
Nycole Xavier <74930052+nycolexavier@users.noreply.github.com>
2022-03-16 12:06:01 -0300

    arrays e loops 1 finish

---
+++ arrays-e-loops/script.js
@@ -0,0 +1,13 @@
+var videoGames = ["Switch", "PS4", "XBOX"];
+
+var ultimoItem = videoGames.pop();
+
+for (let numero = 0; numero < 10; numero++) {
+  console.log(numero);
+}
+
+var i = 0;
+while (i <= 100) {
+  console.log(i);
+  i++;
+}
