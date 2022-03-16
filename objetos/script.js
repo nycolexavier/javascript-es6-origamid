eb079d3cc7404cc103ba1fd5d6f03385316fd16e
Nycole Xavier <74930052+nycolexavier@users.noreply.github.com>
2022-03-16 10:39:44 -0300

    objetos 1 finish

---
+++ objetos/script.js
@@ -0,0 +1,19 @@
+var pessoa = {
+  nome: "André",
+  idade: 28,
+};
+
+var quadrado = {
+  lados: 4,
+  area: function (lado) {
+    return lado * lado;
+  },
+  perimetro: function (lado) {
+    return this.lados * lado;
+  },
+};
+
+console.log(quadrado.area(5));
+console.log(quadrado.perimetro(5));
+
+console.log(Math.random());
