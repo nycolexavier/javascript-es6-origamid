69680c001db2e62195d2f6432dd8dcd9b9bccda1
Nycole Xavier <74930052+nycolexavier@users.noreply.github.com>
2022-03-17 15:29:08 -0300

    escopo finish

---
+++ escopo/srcipt-aula.js
@@ -0,0 +1,16 @@
+var carro = "Fusca";
+
+function mostrarCarro() {
+  var carro = "Fusca";
+  console.log(carro);
+}
+
+mostrarCarro(); // Fusca no console
+console.log(carro); // Erro, carro is not defined
+
+if (false) {
+  var mes = "Dezembro";
+  console.log(mes);
+}
+
+console.log(mes);
